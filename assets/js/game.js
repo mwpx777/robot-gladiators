6997eb7988d70ae3ed1016a3b3c13f746b8845dd
MWPX777 <mwpx777@gmail.com>
2020-12-12 09:05:23 -0600

    added skip or fight on section

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,6 +1,7 @@
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
+var playerMoney = 10;
 
 // You can also log multiple values at once like this
 console.log(playerName, playerAttack, playerHealth);
@@ -12,40 +13,62 @@ var enemyAttack = 12;
 window.alert("Welcome to Robot Gladiators you bitches!");
 
 var fight = function() {
-   enemyHealth = enemyHealth-playerAttack;
-    console.log(playerName + " attacked " + enemyName + "." + enemyName +  " now has " + enemyHealth + " health remaining.");
+    var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle?  Type 'FIGHT' to continue, or 'SKIP' to chicken out.");
+    console.log(promptFight);
 
-    //check enemy health
-    if (enemyHealth <= 0) {
-        window.alert(enemyName + " is wasted.");
-      }
-    else {
-        window.alert(enemyName + " still has " + enemyHealth + " health remaining.");
+    if (promptFight === 'FIGHT' || promptFight === 'fight') {
+        window.alert("You are one tough son of a bitch!  I like that about you!");
+    }
+  
+
+    if (promptFight === "fight" || promptFight === "FIGHT") {
+        //player attack
+        enemyHealth = enemyHealth-playerAttack;
+        console.log(playerName + " attacked " + enemyName + "." + enemyName +  " now has " + enemyHealth + " health remaining.");
+
+        //check enemy health
+        if (enemyHealth <= 0) {
+            window.alert(enemyName + " is wasted.");
+        }
+        else {
+            window.alert(enemyName + " still has " + enemyHealth + " health remaining.");
+        }
+        //enemy attack
+        playerHealth = playerHealth-enemyAttack;
+            console.log(enemyName + " attacked " + playerName + ". " + playerName +  " now has " + playerHealth + " health remaining.");
+
+        // check player's health
+        if (playerHealth <= 0) {
+        window.alert(playerName + " is wasted");
+        } 
+        else {
+            window.alert(playerName + " still has " + playerHealth + " health remaining.");
+        }
+
+    // if player chooses to SKIP
+    }else if (promptFight === "SKIP" || promptFight === "skip"){
+        //confirm player wants to skip
+        var confirmSkip = window.confirm("Are you sure you want to be a wussbag?");
+        //if yes (true), leave fight
+        if (confirmSkip) {
+            window.alert(playerName + " has decided to be a total wussbag and leave the fight.");
+            //subtract money from player
+            playerMoney = playerMoney - 2;
+            console.log(playerMoney)
+            window.alert(playerName + " now has " + playerMoney + " bucks remaining.");
+        }
+        //if no (false), ask question again about running fight() again
+        else {
+            window.alert("Smart choice douchebag!");
+            fight();
+        }
+
+    } else { 
+    window.alert("You need to choose a valid response.  Try again dummy!");
     }
 
-playerHealth = playerHealth-enemyAttack;
-    console.log(enemyName + " attacked " + playerName + ". " + playerName +  " now has " + playerHealth + " health remaining.");
+}
 
-    // check player's health
-    if (playerHealth <= 0) {
-    window.alert(playerName + " is wasted");
-    } 
-    else {
-        window.alert(playerName + " still has " + playerHealth + " health remaining.");
-    }
-
-  };
-
-  //if (playerHealth > 0) {
-    //console.log(playerName + " is still alive!");
-  //}
-
- // if (playerHealth === 0){
-   // console.log("this will not run");
- // }
-
-  //else {
-      //console.log(playerName = " is WASTED!");
-  //}
+ 
 
 fight();
